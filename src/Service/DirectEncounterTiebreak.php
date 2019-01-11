00139a77487cc875e68f464fcdf4ed11b87e9057
LaJul <julien.favarel@gmail.com>
2019-01-11 11:01:20 +0100

    init grille américaine + classement

---
+++ src/Service/DirectEncounterTiebreak.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Service;
+
+use App\Service\TiebreakInterface;
+
+/**
+ * Description of BlackGamesTiebreak
+ *
+ * @author Aspom
+ */
+class BlackGamesTiebreak implements TiebreakInterface
+{
+    public function compare($player1, $player2) {
+        return $player1->getBlackGames() - $player2->getWhiteGames();
+    }
+}
